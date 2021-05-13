1f903b10179e8b930b7e6bc6093ce84c6de30897
satoshi <takemaru33ohayo@yahoo.co.jp>
2021-05-13 13:26:00 +0900

    php練習

---
+++ sample07.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="ja">
+<head>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="css/style.css">
+
+<title>PHP</title>
+</head>
+<body>
+<header>
+<h1 class="font-weight-normal">PHP</h1>    
+</header>
+
+<main>
+<h2>Practice</h2>
+<pre>
+<?php
+date_default_timezone_set('Asia/Tokyo');
+// print (time() + 60 * 60 * 24);
+// print (date('n/j(D)' , time() + 60 * 60 * 24));
+for ($i=1; $i<=365; $i++) {
+  $date = strtotime('+' . $i . 'day');
+  print (date('n/j(D)' , $date));
+  print "\n";
+}
+?>
+</pre>
+</main>
+</body>    
+</html>
