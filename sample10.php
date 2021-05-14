787912fa04efe8d38cef9aee2b97e89a961e8e63
satoshi <takemaru33ohayo@yahoo.co.jp>
2021-05-15 05:53:20 +0900

    配列、if文

---
+++ sample10.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="ja">
+<head>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="css/style.css">
+
+<title>PHP</title>
+</head>
+<body>
+<header>
+<h1 class="font-weight-normal">PHP</h1>    
+</header>
+
+<main>
+<h2>Practice</h2>
+<pre>
+<?php
+print (date('G'));
+
+if (date('G') < 23) {
+print('※　現在受付時間外です');
+} else {
+print('ようこそ')
+}
+
+// if (date('G') >= 23) {
+//   print('※ようこそ');
+//   }
+
+
+
+?>
+</pre>
+</main>
+</body>    
+</html>
