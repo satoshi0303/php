f2a2009eac18ca768da1ba253592c3a3f664c9b9
satoshi <takemaru33ohayo@yahoo.co.jp>
2021-05-23 04:00:38 +0900

    剰余算の応用、クッキーの保存

---
+++ sample23.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="ja">
+<head>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="css/style.css">
+
+<title>PHP</title>
+</head>
+<body>
+<header>
+<h1 class="font-weight-normal">PHP</h1>    
+</header>
+
+<main>
+<h2>Practice</h2>
+<!-- <pre>
+<?php
+$week = array('月', '火', '水', '木', '金', '土', '日');
+for ($i=1; $i<31; $i++) {
+print($week[$i%7] . "\n");
+}
+?>
+</pre> -->
+<table>
+<?php
+for ($i=1; $i<10; $i++){
+  if ($i % 2) {
+    print ('<tr style="background-color: #ccc">');
+  } else {
+    print('<tr>');
+  }
+  print('<td>' . $i . '行目</td>');
+  print('</tr>');
+}
+?>
+  <tr>
+    <td>一行目</td>  
+  </tr>
+  <tr style="background-color: #ccc">
+    <td>一行目</td>
+  </tr>
+</table>
+</main>
+</body>    
+</html>
